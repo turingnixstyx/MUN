090ff3fa889bee7c14fec93794d43549d1ca4fc1
turingnixstyx <naman.mishra194@gmail.com>
2023-08-09 13:57:38 +0530

    prod settings added

--- backend/backend/settings_prod.py
+++ backend/backend/settings_prod.py
@@ -5,3 +5,12 @@ DEBUG=False
 
 ALLOWED_HOSTS = ['45.79.127.173']
 
+INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS
+
+CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_HTTPONLY = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+MIDDLEWARE = ["whitenoise.middleware.WhiteNoiseMiddleware"] + MIDDLEWARE
