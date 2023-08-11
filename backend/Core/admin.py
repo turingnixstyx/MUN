18132cba5198d9ecc0b44accf3bdef2466b22213
turingnixstyx <naman.mishra194@gmail.com>
2023-08-12 01:23:51 +0530

    status added to level-1

--- backend/Core/admin.py
+++ backend/Core/admin.py
@@ -44,9 +44,9 @@ class AllTrackerAdmin(admin.ModelAdmin):
 @admin.register(ImpactChallengeTable)
 class ImpactChallengeAdmin(admin.ModelAdmin):
     form=ImpactModelAdminForm
-    list_display = ["student", "school", 'get_preferences_one', 'get_preferences_two', 'get_preferences_three', "committee", "portfolio", "remarks",]
+    list_display = ["student", "school", 'get_preferences_one', 'get_preferences_two', 'get_preferences_three', "committee", "portfolio", "remarks", "status"]
     list_filter = ["school", "committee", "portfolio"]
-    list_editable = ('committee', 'portfolio',)
+    list_editable = ('committee', 'portfolio', "status")
     actions = ["export_as_csv"]
 
     def get_preferences_one(self, obj):
@@ -92,9 +92,9 @@ class ImpactChallengeAdmin(admin.ModelAdmin):
 @admin.register(MUNChallengeTable)
 class MUNAdmin(admin.ModelAdmin):
     form=MUNModelAdminForm
-    list_display = ["student", "school", 'get_preferences_one', 'get_preferences_two', 'get_preferences_three', "committee", "portfolio", "remarks"]
+    list_display = ["student", "school", 'get_preferences_one', 'get_preferences_two', 'get_preferences_three', "committee", "portfolio", "remarks", 'status']
     list_filter = ["school", "committee", "portfolio"]
-    list_editable = ('committee', 'portfolio',)
+    list_editable = ('committee', 'portfolio', 'status')
     actions = ["export_as_csv"]
 
     def get_preferences_one(self, obj):
