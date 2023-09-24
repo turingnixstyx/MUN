2c6b295ee5ca5628d40be56da87eb19a82549547
Naman Mishra <62857931+turingnixstyx@users.noreply.github.com>
2023-09-24 08:03:30 +0530

    all templates done

--- backend/static/js/preferences.js
+++
@@ -1,7 +0,0 @@
-const inputElement= document.getElementById('id_text');
-
-inputElement.addEventListener('keydown', function(event){
-    if(event.key == "Enter"){
-        event.preventDefault();
-    }
-});
